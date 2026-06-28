de3a049b7199d2041cef43c8a37a71b4c8e46b31
ebenezerjkamishina-code <ebenezerjkamishina@gmail.com>
2026-06-28 15:06:05 +0300

    fix middleware

--- config/settings.py
+++ config/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # 2nd
-    'django.contrib.sessions.middleware.SessionMiddleware',  # <- Must be before Auth
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',  # <- Must be after Sessions
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
